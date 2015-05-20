ae9738f33d627fdd18983eddb18d23e9fa589c23
Volodymyr Partyka <vovuk097@gmail.com>
2015-05-20 17:38:22 +0300

    faye with mustache(create,delete) updated

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -7,29 +7,6 @@ $(function() {
     //    eval(data);
     //});
 
-
-    faye.subscribe("/task/new", function(data) {
-        var allTasks =$.ajax({
-            dataType: "json",
-            url: "/tasks.json",
-            data: data
-        }).done(function(response) {
-        $.each(response, function (i, field) {
-            $("#share").append("<tr id=\"delete-'"+i+"'\">\n  " +
-                "<td><a href=\"/tasks/'"+i+"'\">"+field+"<\/a><\/td>\n  " +
-                "<td><a href=\"/tasks/'"+i+"'\">"+field+"<\/a><\/td>\n  " +
-                "<td>field<\/td>\n  <td>field<\/td>\n  " +
-                "<td>field<\/td>\n  <td>field<\/td>\n  " +
-                "<td><\/td>\n     " +
-                " <td>\n        <a class=\"btn btn-info btn-xs\" " +
-                "href=\"/tasks/253/edit\">Edit<\/a>\n        " +
-                "<a data-confirm=\"Are you sure?\" class=\"btn btn-xs btn-danger\" data-remote=\"true\" rel=\"nofollow\" " +
-                "data-method=\"delete\" href=\"/tasks/253\">Delete<\/a>\n      <\/td>\n<\/tr>");
-
-        });
-    });
-    });
-
     faye.subscribe("/tasks/deleted", function(data) {
 
         $.ajax({
@@ -37,9 +14,9 @@ $(function() {
             url: "tasks.json",
             data: data
         }).done(function(response) {
-            $.each(response, function (i, field) {
-                $("table.table tbody tr").remove();
-            });
+
+            $("table.table tbody tr").remove();
+
             $.each(response, function (i, field) {
                 var task={
                     id: field.id,
@@ -63,29 +40,9 @@ $(function() {
 
                 var html = Mustache.to_html(template, task);
                 $('#share').append(html);
-
-
-            //$("#share").append("<tr id=\"delete-'"+field.id+"'\">\n  " +
-            //    "<td><a href=\"/tasks/'"+field.id+"'\">"+field.id+"<\/a><\/td>\n  " +
-            //    "<td><a href=\"/tasks/'"+field.id+"'\">"+field.key+"<\/a><\/td>\n  " +
-            //    "<td>"+field.priority+"<\/td>\n  <td>"+field.reporter+"<\/td>\n  " +
-            //    "<td>"+field.assignee+"<\/td>\n  <td>"+field.description+"<\/td>\n  " +
-            //    "<td>"+field.created_at+"<\/td>\n     " +
-            //    " <td>\n        <a class=\"btn btn-info btn-xs\" " +
-            //    "href=\"/tasks/"+field.id+"/edit\">Edit<\/a>\n        " +
-            //    "<a data-confirm=\"Are you sure?\" class=\"btn btn-xs btn-danger\" data-remote=\"true\" rel=\"nofollow\" " +
-            //    "data-method=\"delete\" href=\"/tasks/"+field.id+"\">Delete<\/a>\n      <\/td>\n<\/tr>");
             });
         });
     });
-
-
-
-
-
-
-
-
 
     faye.subscribe("/tasks/new", function(data) {
         $.ajax({
@@ -102,7 +59,7 @@ $(function() {
                 description: response[0].description,
                 created_at: response[0].created_at
             };
-            var template = "<tr id=\"delete-'{{id}}'\">\n  " +
+            var template = "<tr id=\"delete-'{{id}}'\">\n " +
                 "<td><a href=\"/tasks/'{{id}}'\">{{id}}<\/a><\/td>\n  " +
                 "<td><a href=\"/tasks/'{{id}}'\">{{key}}<\/a><\/td>\n  " +
                 "<td>{{priority}}<\/td>\n  <td>{{reporter}}<\/td>\n  " +
@@ -119,14 +76,3 @@ $(function() {
     });
 
 });
-
-//$("#share").prepend("<tr id=\"delete-'"+response[0].id+"'\">\n  " +
-//    "<td><a href=\"/tasks/'"+response[0].id+"'\">"+response[0].id+"<\/a><\/td>\n  " +
-//    "<td><a href=\"/tasks/'"+response[0].id+"'\">"+response[0].key+"<\/a><\/td>\n  " +
-//    "<td>"+response[0].priority+"<\/td>\n  <td>"+response[0].reporter+"<\/td>\n  " +
-//    "<td>"+response[0].assignee+"<\/td>\n  <td>"+response[0].description+"<\/td>\n  " +
-//    "<td>"+response[0].created_at+"<\/td>\n     " +
-//    " <td>\n        <a class=\"btn btn-info btn-xs\" " +
-//    "href=\"/tasks/"+response[0].id+"/edit\">Edit<\/a>\n        " +
-//    "<a data-confirm=\"Are you sure?\" class=\"btn btn-xs btn-danger\" data-remote=\"true\" rel=\"nofollow\" " +
-//    "data-method=\"delete\" href=\"/tasks/"+response[0].id+"\">Delete<\/a>\n      <\/td>\n<\/tr>");
